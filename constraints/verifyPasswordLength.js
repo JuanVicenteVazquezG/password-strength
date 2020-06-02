07dc3ab7ca6e7f3ff22aff5823e6b500d67f7302
Juan Vicente Vazquez <juanvicentevazquezg@gmail.com>
2020-06-02 13:28:11 +0200

    uso del modulo de verificacion

---
+++ constraints/verifyPasswordLength.js
@@ -0,0 +1,9 @@
+module.exports = function (password) {
+    if (password.length < 6) {
+        return Promise.reject({
+            message: ' El password es demasiado corto'
+        })
+    }
+    return Promise.resolve('El password pasa la validacion');
+    
+}
--- handler.js
+++ handler.js
@@ -1,18 +1,25 @@
 'use strict';
 
-module.exports.hello = async event => {
-  return {
-    statusCode: 200,
-    body: JSON.stringify(
-      {
-        message: 'Go Serverless v1.0! Your function executed successfully!',
-        input: event,
-      },
-      null,
-      2
-    ),
-  };
+const verifyPasswordLength = require('./constraints/verifyPasswordLength');
 
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
+module.exports.password = async event => {
+  try {
+    const { password } = event.pathParameters;
+    await verifyPasswordLength(password);
+    return {
+      statusCode: 200,
+      body: JSON.stringify({
+        message: 'Todo OK!',
+      })
+    }
+  }
+  catch (e) {
+    return {
+      statusCode: 400,
+      body: JSON.stringify({
+        message: `Error: ${e.message}`,
+      })
+    }
+  }
+
 };
